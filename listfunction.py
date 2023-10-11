808b2e1a44dd171e568802f73b049d29c5b1bcec
adrian pawlowski <adrian.pawlowski08@gmail.com>
2023-10-11 19:43:23 +0000

    commit

--- listfunction.py
+++ listfunction.py
@@ -20,3 +20,16 @@ secList = [ "Six", "Seven", "Eight", "Nine" ]
 mylist.extend(secList)
 print(mylist)
 print(len(mylist))
+
+# Removing Value from List
+
+# Remove By Name
+mylist.remove("Nine")
+print(mylist)
+print(len(mylist))
+
+# Remove by Index
+mylist.remove(7)
+print(mylist)
+print(len(mylist))
+
